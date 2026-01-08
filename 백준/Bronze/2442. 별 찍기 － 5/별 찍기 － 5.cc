2b2178d8b6e227acf11240a2359850658f90ed1b
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2026-01-08 10:58:02 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < n - (i+1); k++){
+            cout << " ";
+        }
+        for(int j = 0; j < (2*i) + 1; j++){
+            cout << "*";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
