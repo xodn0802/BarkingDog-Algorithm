07be8159049222468036602b3f740ba6548669f7
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2026-01-13 17:32:54 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,63 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 1000005;
-int dat[MX];
-int pos = 0;
-
-void push(int x){
-    dat[pos] = x;
-    pos++;
-}
-
-bool empty(){
-    if(pos == 0){
-        return true;
-    }return false;
-}
-
-int pop(){
-    if(empty()) return -1;
-    pos--;
-    return dat[pos];
-}
-
-int size(){
-    return pos;
-}
-
-
-
-int top(){
-    if(empty()) return -1;
-    return dat[pos-1];
-}
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, m;
+
+    int n,m;
     string s;
-
+    stack<int> st;
     cin >> n;
-
     for(int i = 0; i < n; i++){
         cin >> s;
         if(s == "push"){
             cin >> m;
-            push(m);
+            st.push(m);
         }else if(s == "pop"){
-            cout << pop() << '\n';
-        }else if(s == "size"){
-            cout << size() << '\n';
-        }else if(s == "empty"){
-            if(empty()) cout << 1 << '\n';
-            else{
-                cout << 0 << '\n';
+            if(st.empty()){
+                cout << -1 << '\n';
+            }else{
+                cout << st.top() << '\n';
+                st.pop();
             }
+        }else if(s == "size"){
+            cout << st.size() << '\n';
+        }else if(s == "empty"){
+            cout << st.empty() << '\n';
         }else{
-            cout << top() << '\n';
+            if(st.empty()){
+                cout << -1 << '\n';
+            }else{
+                cout << st.top() << '\n';
+            }
+            
         }
     }
+
     return 0;
 }
