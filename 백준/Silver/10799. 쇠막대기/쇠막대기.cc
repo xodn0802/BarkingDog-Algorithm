4da160ccc40ee354a7c54c780b2fa61045bb9474
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2026-01-19 16:10:23 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    stack<char> st;
+    int cnt = 0;
+    cin >> s;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == '('){
+            st.push(s[i]);
+        }else if(s[i] == ')'){
+            if(s[i-1] == '('){
+                st.pop();
+                cnt += st.size();
+            }else{
+                st.pop();
+                cnt += 1;
+            }
+        }
+    }
+    cout << cnt;
+
+    return 0;
+}
