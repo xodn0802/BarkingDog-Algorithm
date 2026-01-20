969c3e6bd0000ee4b5a909468a76dbd20b2070a2
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2026-01-20 14:29:59 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -17,25 +17,23 @@ int main(){
         cin >> board[i];
     }
     for(int i = 0; i < n; i++){
-        fill(dist[i], dist[i] + m,-1);
+        fill(dist[i], dist[i] + m, -1);
     }
     queue<pair<int,int>> Q;
     Q.push({0,0});
     dist[0][0] = 0;
     while(!Q.empty()){
-        auto cur = Q.front();
-        Q.pop();
+        auto cur = Q.front(); Q.pop();
         for(int dir = 0; dir < 4; dir++){
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m){
-                continue;
-            }
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
             if(dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
             dist[nx][ny] = dist[cur.X][cur.Y] + 1;
             Q.push({nx,ny});
         }
     }
     cout << dist[n-1][m-1] + 1;
+
     return 0;
 }
